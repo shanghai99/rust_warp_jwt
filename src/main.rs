d5e806b5a0e71f92dd5a89ae23acc2d0301d844a
Shanghai99 <geoffrey.shanghai@gmail.com>
2023-09-26 01:40:39 +0800

    third commit

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
-use std::clone;
+use auth::{with_auth, Role};
+use error::Error::*;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::convert::Infallible;
+use std::sync::Arc;
+use warp::{reject, reply, Filter, Rejection, Reply};
 
-#[derive(clone)]
+mod auth;
+mod error;
+
+#[derive(Clone)]
 pub struct User {
     pub uid: String,
     pub email: String,
@@ -18,4 +26,30 @@ pub struct LoginRequest {
 #[derive(Serialize)]
 pub struct LoginResponse {
     pub token: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let users = Arc::new(init_users());
+
+    let login_route = warp::path!("login")
+        .and(warp(post()))
+        .and(with_users(users.clone()))
+        .and(warp::body::json())
+        .and_then(login_handler);
+
+    let user_route = warp::path!("user")
+        .and(with_auth(Role::User))
+        .and_then(user_handler);
+
+    let admin_route = warp::path!("admin")
+        .and(with_auth(Role::Admin))
+        .and_then(admin_handler);
+
+    let routes = login_route
+        .or(user_route)
+        .or(admin_route)
+        .recover(error::handle_rejection);
+
+    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
 }
